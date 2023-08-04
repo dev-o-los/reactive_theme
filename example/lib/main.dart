08b028572e6a7fa2aed98058489326f96e8955e3
devolos <utk27032005@gmail.com>
2023-08-04 11:11:43 +0530

    new example folder added

---
+++ example/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_theme/reactive_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Wrap with [Reactive Themer] Widget
+    return ReactiveThemer(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Reactive Theme Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              brightness: Brightness.light, seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              brightness: Brightness.dark, seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //Change Theme Using these [ReactiveSwitch]
+          const Center(child: ReactiveSwitch()),
+          const SizedBox(height: 20),
+          //Change Theme Using these [ReactiveThemeBtn]
+          const Center(child: ReactiveThemeBtn()),
+          const SizedBox(height: 20),
+          //Accessible Bool for checking isDarkmode and !isDarkmode
+          ReactiveMode.isDarkMode(context)
+              ? const Text('is darkmode')
+              : const Text('is not darkmode'),
+        ],
+      ),
+    );
+  }
+}
