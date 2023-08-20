1b530f3ae0256e9e1b8bde58643184467d4d8335
devolos <utk27032005@gmail.com>
2023-08-20 16:26:26 +0530

    Commit Message: 	 - Add constructor and parameters to ReactiveThemeBtn class. Commit Description:  - This MR adds a constructor and several optional named parameters with default values to the ReactiveThemeBtn class, allowing for customization of the button's background color, icon color, transition animation, icon order, and icons for light and dark modes.

--- lib/src/exports/reactive_theme_btn.dart
+++ lib/src/exports/reactive_theme_btn.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../private/theme_mutator.dart';
 
 class ReactiveThemeBtn extends StatelessWidget {
+  /// The code is defining a constructor for the `ReactiveThemeBtn` class. The constructor takes several
+  /// optional named parameters with default values.
   const ReactiveThemeBtn({
     super.key,
     this.bgColWhenLg = Colors.white,
@@ -14,25 +16,49 @@ class ReactiveThemeBtn extends StatelessWidget {
     this.reverse = false,
   });
 
+  /// `bgColWhenLg` is a named parameter in the constructor of the `ReactiveThemeBtn`
+  /// class. It represents the background color of the button when the app is in light
+  /// mode. By default, it is set to `Colors.white`.
   final Color? bgColWhenLg;
 
+  /// `bgColWhendk` is a named parameter in the constructor of the `ReactiveThemeBtn` class.
+  /// It represents the background color of the button when the app is in dark mode. By
+  /// default, it is set to `Colors.black87`.
   final Color? bgColWhendk;
 
+  /// `iconColWhenLg` is a named parameter in the constructor of the `ReactiveThemeBtn`
+  /// class. It represents the color of the icon on the button when the app is in light
+  /// mode. By default, it is set to `Colors.black87`.
   final Color? iconColWhenLg;
 
   final Color? iconColWhendk;
 
+  /// The `transitionType` parameter in the `ReactiveThemeBtn` class is a function that defines the
+  /// transition animation for the icon when the theme mode is toggled. It takes a `Widget` child and
+  /// an `Animation<double>` animation as parameters and returns a new `Widget` that represents the
+  /// animated transition.
   final Widget Function(Widget child, Animation<double> animation)?
       transitionType;
 
+  /// The `reverse` parameter in the `ReactiveThemeBtn` class is a boolean value that
+  /// determines the order of the icons displayed on the button.
   final bool? reverse;
 
+  /// The `darkmodeIcon` is a named parameter in the constructor of the
+  /// `ReactiveThemeBtn` class. It represents the icon that will be displayed on the
+  /// button when the app is in dark mode. The default value is set to
+  /// `Icons.dark_mode`.
   final IconData? darkmodeIcon;
 
+  /// The `lightmodeIcon` is a named parameter in the constructor of the
+  /// `ReactiveThemeBtn` class. It represents the icon that will be displayed on the
+  /// button when the app is in light mode. The default value is set to `Icons.sunny`.
   final IconData? lightmodeIcon;
 
   @override
   Widget build(BuildContext context) {
+    /// `final thememutator = ThemeMutator.of(context);` is assigning the value returned by the `of`
+    /// method of the `ThemeMutator` class to the variable `thememutator`.
     final thememutator = ThemeMutator.of(context);
     return Container(
       decoration: BoxDecoration(
